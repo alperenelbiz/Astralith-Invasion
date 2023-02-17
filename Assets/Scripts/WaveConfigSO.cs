11577e865d210e2b7208c993f3ee20e840ce084f
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2023-02-17 14:11:01 +0300

    0.02V

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] List<GameObject> enemyPrefabs;
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 10f;
+
+    [Header("Spawner Variables")]
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance;
+    [SerializeField] float minSpawnTime = 0.2f;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+
+        foreach (Transform child in pathPrefab)
+        {
+            waypoints.Add(child);
+        }
+
+        return waypoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance,
+                                        timeBetweenEnemySpawns + spawnTimeVariance);
+
+        return Mathf.Clamp(spawnTime, minSpawnTime, float.MaxValue);
+    }
+}

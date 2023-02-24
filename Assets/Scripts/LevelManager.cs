c5d83eaed370e4d16d661d713ac408b1f704d7e3
H. Alperen Elbiz <94225207+alperenelbiz@users.noreply.github.com>
2023-02-24 15:41:31 +0300

    0.1V
    It is playable, but at a basic level. I will add new enemies, bosses, upgrade items, etc. in the future.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] float sceneLoadDelay = 2f;
+
+    ScoreKeeper scoreKeeper;
+
+    void Awake()
+    {
+    }
+
+    public void LoadGame()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+        scoreKeeper.ResetScore();
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad("Game Over", sceneLoadDelay));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator WaitAndLoad(string sceneName, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneName);
+    }
+}
